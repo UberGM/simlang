176bf8b81d39120de3c62aa1660e9a29397e8a2f
Ryan Beltz <ryanbeltzbackup@gmail.com>
2015-06-28 16:21:24 -0400

    Delete generate.rb

--- generate.rb
+++
@@ -1,9 +0,0 @@
-File.open("main.rb", "w+") { |file| file.write("require_relative 'simlang'
-$lv1 = nil
-$mv1 = nil
-sys = Sys.new
-math = Minimath.new
-
-sys.init("Title", "Author")
-#Your code here!
-sys.end") }
